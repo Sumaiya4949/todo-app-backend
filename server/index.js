b1ef9960500807a62c737a8096374802ef69cd69
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-05-05 18:02:20 +0200

    Implement REST API #3

--- server/index.js
+++ server/index.js
@@ -1,7 +1,38 @@
 const express = require("express");
+const { v4: generateID } = require("uuid");
+const pgp = require("pg-promise")();
+
+const dbConnSettings = {
+  host: "localhost",
+  port: 9000,
+  database: "postgres",
+  user: "postgres",
+  password: "sinthy4949",
+};
+
+const db = pgp(dbConnSettings);
 
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+
+app.put("/api/register", async (req, res) => {
+  const email = req.body.email;
+  const passwordHash = req.body.passwordHash;
+  const fullName = req.body.fullName;
+  const id = generateID();
+
+  await db.any(`
+    INSERT INTO AUTH VALUES ('${email}', '${passwordHash}', '${id}');
+  `);
+
+  await db.any(`
+    INSERT INTO APP_USER VALUES ('${id}', '${fullName}');
+  `);
+
+  res.send({ success: true, id });
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
