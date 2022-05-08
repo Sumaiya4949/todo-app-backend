5fcdb23549caf973232a5482b4a12abdfc78300b
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-05-08 22:34:34 +0200

    Implement API 6

--- server/index.js
+++ server/index.js
@@ -93,3 +93,21 @@ app.post("/auth/logout", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+app.get("/auth/who-am-i", async (req, res) => {
+  const sid = req.cookies.sid;
+  const [userAppSessionData] = await db.any(
+    `SELECT U_ID FROM APP_SESSION WHERE S_ID='${sid}';`
+  );
+
+  const [appUserData] = await db.any(
+    `SELECT FULLNAME FROM APP_USER WHERE ID='${userAppSessionData.u_id}';`
+  );
+
+  res.send({
+    user: {
+      fullname: appUserData.fullname,
+      id: userAppSessionData.u_id,
+    },
+  });
+});
